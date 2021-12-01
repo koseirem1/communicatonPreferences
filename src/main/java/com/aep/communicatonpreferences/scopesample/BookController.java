eef54f0ca4450603aaee3bb4fe063e510b5733ed
koseirem1 <koseirem1@gmail.com>
2021-12-01 12:30:24 +0300

    bean life cycle

--- src/main/java/com/aep/communicatonpreferences/scopesample/BookController.java
+++ src/main/java/com/aep/communicatonpreferences/scopesample/BookController.java
@@ -1,24 +1,52 @@
 package com.aep.communicatonpreferences.scopesample;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-    @RestController
+
+@RestController
     @Scope(WebApplicationContext.SCOPE_SESSION)
 //Scope(WebApplicationContext.SCOPE_REQUEST)
 //@Scope("request")
-    public class BookController {
+    public class BookController implements InitializingBean, DisposableBean {
 
         public BookController(){
+
             System.out.println("REST CONTROLLER --> BOOK Constructor Executed!!");
         }
 
         @GetMapping("/sayhi")
         public String sayHi(){
+
             return "Hi Folks!";
         }
+
+        @PostConstruct
+        public void postCons(){
+            System.out.println("REST CONTROLLER --> BOOK Post Consturtor executed");
+        }
+
+        @PreDestroy
+        public void preDestroy(){
+            System.out.println("REST CONTROLLER --> BOOK Pre Destroy executed");
+        }
+
+        @Override
+        public void afterPropertiesSet() throws Exception {
+            System.out.println("REST CONTROLLER --> BOOK afterPropertiesSet executed");
+        }
+
+        @Override
+        public void destroy() throws Exception {
+            System.out.println("REST CONTROLLER --> BOOK destroy executed");
+        }
+
     }
 
